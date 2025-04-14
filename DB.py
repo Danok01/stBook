2a42299491e31f1b8771e0a432ceae4de3b4973e
Danok01 <dhaniroyal01@gmail.com>
2025-04-14 21:37:39 +0100

    new commit

--- DB.py
+++ DB.py
@@ -1,42 +1,91 @@
-import psycopg2
+# import psycopg2
+# import streamlit as st
 from dotenv import load_dotenv
 import os
-#from app import gmail
+# from supabase import create_client, Client
+import psycopg2
+# from app import gmail
+# from datetime import datetime
 
 # Load environment variables from .env
 load_dotenv()
 
-#DB_URL= ("postgresql://postgres.zfifhntsxhzjlsgofkgf:[YOUR-PASSWORD]@aws-0-eu-central-1.pooler.supabase.com:5432/postgres")
-def get_db(gmail):
-    # Fetch variables
-    USER = os.getenv("user")
-    PASSWORD = os.getenv("password")
-    HOST = os.getenv("host")
-    PORT = os.getenv("port")
-    DBNAME = os.getenv("dbname")
 
-    # Connect to the database
-    try:
-        connection = psycopg2.connect(
-            user=USER,
-            password=PASSWORD,
-            host=HOST,
-            port=PORT,
-            dbname=DBNAME
-        )
-        print("Connection successful!")
-        # Create a cursor to execute SQL queries
-        cursor = connection.cursor()
-        #insert into table
-        print(str(gmail));
-        cursor.execute("INSERT INTO DB_user_lead (email) VALUES (%s)", str(gmail))
-        connection.commit
-        # Close the cursor and connection
-        cursor.close()
-        connection.close()
-        # print("Connection closed.")
+# Connect to the database
+def get_db():
+    conn=psycopg2.connect(
+    user=os.getenv("user"),
+    password=os.getenv("password"),
+    host=os.getenv("host"),
+    port=os.getenv("port"),
+    dbname=os.getenv("dbname")
+    )
+    return conn
+# cur= conn.cursor()
+# get_data= cur.execute("SELECT * FROM DB_user_lead;")
 
-    except Exception as e:
-        print(f"Failed to connect: {e}")
+# get_db(cur.execute("SELECT * FROM users;"))
+# def insert_data():
+#     cur.execute("INSERT INTO users (email) VALUES (%(gmail))")
+#     cur.close
+
+# def update_rows():
+#     cur.execute("UPDATE users WHERE email= (gmail)")
+#     cur.close
+
+# Create a cursor to execute SQL queries
+# cursor = connection.cursor()
+# insert into table
+# print(gmail)
+# cursor.execute("INSERT INTO DB_user_lead (email) VALUES (%s)", str(gmail))
+# connection.commit
+# Close the cursor and connection
+# cursor.close()
+# connection.close()
+# print("Connection closed.")
+# #DB_URL= ("postgresql://postgres.zfifhntsxhzjlsgofkgf:[YOUR-PASSWORD]@aws-0-eu-central-1.pooler.supabase.com:5432/postgres")
+
+# url: str = os.getenv("SUPABASE_URL")
+# key: str = os.getenv("SUPABASE_API_KEY")
+# supabase: Client = create_client(url, key)
+
+# def insert_to_table(gmail):
+#     existing_data = supabase.table('DB_user_lead').select("*").filter("email", "eq", gmail).execute()
+
+#     if not existing_data:
+#         # Data does not exist, proceed to insert
+#         supabase.table('DB_user_lead').insert({"email": gmail}).execute()
+#     else:
+#         st.write("Data already exists")
 
 
+# def update_table(feedback, rating):
+#     supabase.table('DB_user_lead').update({"feedback": feedback, "rating": rating}).execute()
+"-------------------------------------------------------------"
+# def download_btn():
+#     """Downloads a pre-made PDF file."""
+
+#     # 1. Path to your PDF file (replace with your actual file path)
+#     pdf_file_path = "Creativ-Writing.pdf" # Place your pdf file in the same directory as the python script.
+#     time= datetime.now()
+#     formatted_date = time.strftime("%Y%m%d_%H:%M:%S")
+#     # Check if the PDF file exists
+#     if os.path.exists(pdf_file_path):
+#         try:
+#             # 2. Read the PDF file in binary mode
+#             with open(pdf_file_path, "rb") as pdf_file:
+#                 pdf_bytes = pdf_file.read()
+            
+#             # 3. Streamlit download button
+#             st.download_button(
+#                 label="Download PDF Report",
+#                 data=pdf_bytes,
+#                 file_name=f"DBeng_{formatted_date}.pdf",
+#                 mime="application/pdf",
+#             )
+#         except FileNotFoundError:
+#             st.error("PDF file not found.")
+#         except Exception as e:
+#             st.error(f"An error occurred: {e}")
+#     else:
+#         st.error("PDF file not found in the specified path.")
--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 import streamlit as st
-import DB
+from DB import get_db #get database conn
+from psycopg2 import sql
+from datetime import datetime
 
-#st.set_page_config(page_title="DBE", layout="centered") # webpage name
+#page configuration settings and page setup
+st.set_page_config(page_title="DBE", layout="centered") # webpage name
+css= """
+<style>
+section.stMain.st-emotion-cache-bm2z3a.ea3mdgi8(
+background-image: url("https://plus.unsplash.com/premium_photo-1668473366952-45f06fbf6492?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+background-size: cover;
+)
+</style>"""
+st.markdown(css, unsafe_allow_html=True)
 
+# global variables
+time= datetime.now()
+formatted_date = time.strftime("%Y%m%d_%H:%M:%S")
+pdf_file_path= "DBE.pdf"
+
+#Page writeup
 st.title("Database Engineering")
 st.write(
 """
@@ -11,16 +28,56 @@ a serving point to direct your attention on the most important parts of Database
 Management and Administration. The field being versatile has a twin which is
 Database Development and they should not be confused. Read and Share.
 """)
+
+#Email input and button
 gmail=st.text_input("""
 Enter Gmail and download. Do come back to leave a comment and review anytime.
 """, placeholder= "Enter mail")
+submit_mail=st.button("Submit") 
+
+if submit_mail:
+    if gmail:
+        with open(pdf_file_path, "rb") as pdf_file:
+            pdf_bytes = pdf_file.read()
+        st.download_button(
+            label="Download PDF Report",
+            data=pdf_bytes,
+            file_name=f"DBeng_{formatted_date}.pdf",
+            mime="application/pdf",
+        )
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute(sql.SQL("INSERT INTO users (email) VALUES (%s);"), [gmail])
+        conn.commit()
+        st.success(f"inserted email: {gmail}")
+
+    else:
+        st.error("Please fill in your E-mail")
+    if cur:
+        cur.close()
+    if conn:
+        conn.close()
 
 
-if st.button("Submit"):
-    st.button("Download PDF")
-    #st.download_button(label="Download PDF", file_name="DBeng.pdf")
+@st.dialog("Response/Feedback")
+def submit_response():
+    feedback= st.text_area(label="Enter Feedback", placeholder="My Review")
+    rating= st.text_input(label="Rate Over 10", max_chars=2)
+    response= st.button("Submit Response")
 
-    st.text_area(label="Enter Feedback", placeholder="My Review")
-    st.number_input(label="Rate Over 10", min_value=1, max_value=10)
-    st.button("Submit Response")
-    DB.get_db(gmail)
+    if response:
+        if feedback and rating:
+            if gmail:
+                conn = get_db()
+                cur = conn.cursor()
+                cur.execute(sql.SQL("UPDATE users SET feedback = %s, rating = %s WHERE email = %s;"), [feedback, rating, gmail])
+                conn.commit()
+                cur.close()
+                conn.close()
+            else:
+                st.error("Please fill in your E-mail from Main Page")
+            
+
+        
+if st.button("Submit Review"):
+    submit_response()
